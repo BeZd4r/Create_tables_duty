3374e3e8cee8a59cfdd9bd2926d0f9b507b9fce7
BeZd4r <103284584+BeZd4r@users.noreply.github.com>
2024-10-11 04:39:16 +0300

    Some_debbugs
    Some debbugs for future work

--- main.py
+++ main.py
@@ -20,24 +20,55 @@ days = days[-1] + 1
 Fill_cell_style = PatternFill(patternType='solid',
                        fgColor='808080')
 
-def Create_tables():
+deficient_rooms_list = dict()
+
+def Create_tables(deficient_rooms_list):
     for floors in all_rooms.keys():
-        
+
+        deficient_rooms_list[floors] = {}
+
         for wing in all_rooms[floors].keys():
 
             wb = Workbook()
             sheet = wb.active
 
             i = 1
+
             for room in all_rooms[floors][wing]:
+
+                isWrite = True
                 i += 1
-                cell = sheet.cell(row=1, column=i) 
-                cell.value = room
+                peoples = all_rooms[floors][wing][room]
+                if peoples < 3:
+                        deficient_rooms_list[floors][room] = {"Peoples": peoples, "Column": i}
+                        deficient_rooms_list[floors] = sorted(deficient_rooms_list[floors].items(), key=lambda item:item[1])
+                        deficient_rooms_list[floors] = dict(deficient_rooms_list[floors])
+                        print(deficient_rooms_list)
+                        if deficient_rooms_list[floors] != {}:
+                            stop_flag = False
+                            for deficient_rooms in deficient_rooms_list[floors]:
+                                print(list(deficient_rooms_list[floors][deficient_rooms].values()))
+                                for count in deficient_rooms:
+                                    print(count)
+                                    if int(count) + peoples < 3:
+                                        pass
+                                    elif int(count) + peoples == 3:
+                                        pass
+                                    else:
+                                        continue
+                        else:
+                            cell = sheet.cell(row=1, column=i) 
+                            cell.value = room  
+                            isWrite = False
+                if isWrite:              
+                    cell = sheet.cell(row=1, column=i) 
+                    cell.value = room
 
             for day in range(1,days):
                 cell = sheet.cell(row=day+1, column=1) 
                 cell.value = f'{day}.{current_month}'
             wb.save(filename=f'{floors}/{wing}.xlsx')
+            
 def Paint_cells():
     for floors in all_rooms.keys():
         
@@ -60,7 +91,10 @@ def Paint_cells():
                     if room_value == last_duty_rooms["last_duty_list"][floors][wing]:
                         duty_room = sheet.cell(row=dates, column=room)
                         duty_room.fill = Fill_cell_style
+                        
             wb.save(filename=f'{floors}/{wing}.xlsx')                
        
-Create_tables()
+Create_tables(deficient_rooms_list)
 Paint_cells()
+
+print(deficient_rooms_list)
